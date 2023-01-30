0fbd6f022b1ce5627ffec2ae2cba90135ca8e6b3
andres <andres@doersdf.com>
2023-01-30 12:37:56 +0100

    add testing endpoint

---
+++ src/api/modules/testing/testing.controller.ts
@@ -0,0 +1,7 @@
+import { videos } from '../../db';
+import { Request, Response } from 'express';
+
+export async function deleteAllDataHandler(req: Request, res: Response) {
+  videos.length = 0;
+  res.sendStatus(204);
+}
---
+++ src/api/modules/testing/testing.route.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { deleteAllDataHandler } from './testing.controller';
+
+export const TestingRouter = Router();
+
+TestingRouter.delete('/all-data', deleteAllDataHandler);
