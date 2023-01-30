eda4e6d491364f3fa77cbb5dd99567b1243f6faf
andres <andres@doersdf.com>
2023-01-30 12:40:50 +0100

    add testing endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { videoRouter } from './modules/video/video.route';
+import { TestingRouter } from './modules/testing/testing.route';
 
 const router = express.Router();
 
 router.use('/videos', videoRouter);
+router.use('/testing', TestingRouter);
 export default router;
